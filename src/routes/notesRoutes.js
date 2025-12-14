6b5e624f85d4950ca26a279dc021a7363d5cca48
kristinka-skl <kristinka.skl@gmail.com>
2025-12-14 18:49:23 +0100

    add authenticate

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -2,8 +2,11 @@ import { Router } from 'express';
 import { getAllNotes, getNoteById, createNote, deleteNote, updateNote } from '../controllers/notesController.js';
 import { getAllNotesSchema, noteIdSchema, createNoteSchema, updateNoteSchema } from '../validations/notesValidation.js';
 import { celebrate } from 'celebrate';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
+
+router.use('/notes', authenticate);
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema),getNoteById);
